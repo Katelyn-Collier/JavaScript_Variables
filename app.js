e4f65ec11d223cfc9cd8426e8e690db4930cd2c3
Katelyn-Collier <108428130+Katelyn-Collier@users.noreply.github.com>
2023-05-11 00:11:01 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+//console.log("Hello World!\n==========\n");
+//console.log(
+//  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+//);
 
 // Exercise 1
+const firstName = "Katelyn";
+let lastName = "Collier";
+
+var age = 20
 
 // Exercise 2
+let fullName = firstName + "" + lastName;
 
+//bonus
+let templateFullName = '${firstName} ${lastName}';
+
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 // Exercise 3
+let myStory;
+let city = "Dora";
+let pastTime = "reading, studying, gardening";
+
+myStory = 'My name is ${fullName}. I live in ${city} and spend most of my time ${pastTime}, and coding!'
+console.log(myStory);
